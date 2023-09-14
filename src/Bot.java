ded2941b85b870e1ec31a5d8f9c78bd0ff324d7d
mrsyaban <mrsyaban99@gmail.com>
2023-09-14 12:30:59 +0700

    edit setup

--- src/Bot.java
+++ src/Bot.java
@@ -3,7 +3,7 @@ import javafx.scene.control.Button;
 public class Bot {
     public int[] move(Button[][] buttons) {
         // create random move
-        int[] = new int[]{(int) (Math.random()%8), (int) (Math.random()%8)};
+//        int[] = new int[]{(int) (Math.random()%8), (int) (Math.random()%8)};
 
         return new int[]{(int) (Math.random()%8), (int) (Math.random()%8)};
     }
