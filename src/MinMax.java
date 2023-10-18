61099988fecdd974e45035a8d6ecbd2b0e1c9560
Fajar Herawan <13521080@std.stei.itb.ac.id>
2023-10-18 18:03:56 +0700

    Update: MinMAX

--- src/MinMax.java
+++ src/MinMax.java
@@ -1,10 +1,16 @@
 import javafx.scene.control.Button;
 
+import java.time.LocalDateTime;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
 /**
  * Class to run a bot according to the MinMax Alpha Beta Pruning algorithm
  */
 public class MinMax extends BotController{
-    private final int defaultMaxDepth = 4; // default maximum depth of child generating
+    private final int defaultMaxDepth = 5; // default maximum depth of child generating
     private int MaxDepth; // leaf's depth
     private int[] selected = new int[2]; // [x,y]
 
@@ -12,7 +18,6 @@ public class MinMax extends BotController{
         this.currentState = map;
         this.playerXTurn = playerXTurn;
         this.roundsLeft = roundsLeft;
-        this.MaxDepth = Math.min(defaultMaxDepth, roundsLeft);
         this.selected[0] = -1;
         this.selected[1] = -1;
     }
@@ -25,14 +30,19 @@ public class MinMax extends BotController{
      * @return heuristic value
      */
     private double miniMaxAB(int depth, Button[][] map, double alpha, double beta){
+        this.MaxDepth = Math.min(defaultMaxDepth, this.roundsLeft);
+
         // leaf node
         if(depth == this.MaxDepth){
             try {
                 //
                 if (this.roundsLeft == depth) {
+                    System.out.println("halo");
+                    System.out.println(ObjectiveFunction(0,map));
+                    System.out.println(alpha);
                     return ObjectiveFunction(0, map);
                 } else if (depth%2 == 1){
-                    // Minimum
+                    // Minimum]
                     return ObjectiveFunction(-1, map);
                 }
                 // Maximum
@@ -95,7 +105,22 @@ public class MinMax extends BotController{
 
     @Override
     public int[] run() {
-        double superMaxValue = miniMaxAB(0, this.currentState, Double.NEGATIVE_INFINITY, Double.POSITIVE_INFINITY);
+
+        ExecutorService executorService = Executors.newSingleThreadExecutor();
+        CompletableFuture<Void> future = CompletableFuture.runAsync(() -> {
+            double superMaxValue = Double.NEGATIVE_INFINITY;
+            superMaxValue = miniMaxAB(0, this.currentState, Double.NEGATIVE_INFINITY, Double.POSITIVE_INFINITY);
+        }, executorService);
+
+        try {
+            future.get(5, TimeUnit.SECONDS); // Wait for up to 5 seconds
+        } catch (Exception e) {
+            // Handle timeout or exceptions
+            future.cancel(true); // Cancel the task if it takes too long
+        }
+        executorService.shutdown();
+
+        this.roundsLeft -= 1;
         return this.selected;
     }
 
